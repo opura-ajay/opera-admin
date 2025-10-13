a4d1802285bf7cb7f5bc1d0326f2ba8209036f9f
Ajay Satre <ajay@MacBookPro.lan>
2025-10-13 09:39:25 +0530

    ch

--- src/app/frontend/(admin)/dashboard/new-page.tsx
+++ src/app/frontend/(admin)/dashboard/new-page.tsx
@@ -1,15 +1,15 @@
-import Footer from "../../components/layout/Footer"
-import Header from "../../components/layout/Header"
+// import Footer from "../../components/layout/Footer"
+// import Header from "../../components/layout/Header"
 
-export default function DashboardPage() {
+// export default function DashboardPage() {
 
-  return (
-    <>
-    <Header />
-    <div>
-        <h1>Dashboard work here .....!</h1>
-    </div>
-    <Footer />
-    </>
-  )
-}
+//   return (
+//     <>
+//     <Header />
+//     <div>
+//         <h1>Dashboard work here .....!</h1>
+//     </div>
+//     <Footer />
+//     </>
+//   )
+// }
